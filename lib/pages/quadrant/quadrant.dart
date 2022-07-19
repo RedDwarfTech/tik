ba2a622622f1b5ddeffafa92ded26e6f64db4c75
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-07-19 09:36:35 +0800

    fix: quadrant scroll problem

--- lib/pages/quadrant/quadrant.dart
+++ lib/pages/quadrant/quadrant.dart
@@ -10,32 +10,34 @@ class Quadrant extends StatelessWidget {
   Widget build(BuildContext context) {
     var itemWidth = (MediaQuery.of(context).size.width - 30) / 2;
     var itemHeight = MediaQuery.of(context).size.height;
-    var childAspectRatio = itemWidth / (itemHeight / 2 - 66);
 
     return GetBuilder<QuadrantController>(
         init: QuadrantController(),
         builder: (controller) {
-          return Scaffold(
-              body: SafeArea(
-                  child: GridView.custom(
-            semanticChildCount: 2,
-            cacheExtent: 4,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              mainAxisSpacing: 3.0,
-              crossAxisSpacing: 3.0,
-              crossAxisCount: 2,
-              childAspectRatio: childAspectRatio,
-            ),
-            childrenDelegate: SliverChildBuilderDelegate((BuildContext context, int index) {
-              return DragTarget(onAccept: (data) {
-                TodoTask task = data as TodoTask;
-                task.priority = index + 1;
-                TaskProvider.updateTaskProperties(task).then((value) => {controller.getTodoTasks()});
-              }, builder: (context, candidateData, rejectedData) {
-                return controller.buildListViewItemWidget(
-                    index, controller.tasks.where((element) => element.priority == index + 1).toList());
-              });
-            }, childCount: 4),
+          return Scaffold(body: SafeArea(child: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              var childAspectRatio = itemWidth / (constraints.maxHeight / 2 - 20);
+              return GridView.custom(
+                semanticChildCount: 2,
+                cacheExtent: 4,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  mainAxisSpacing: 3.0,
+                  crossAxisSpacing: 3.0,
+                  crossAxisCount: 2,
+                  childAspectRatio: childAspectRatio,
+                ),
+                childrenDelegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+                  return DragTarget(onAccept: (data) {
+                    TodoTask task = data as TodoTask;
+                    task.priority = index + 1;
+                    TaskProvider.updateTaskProperties(task).then((value) => {controller.getTodoTasks()});
+                  }, builder: (context, candidateData, rejectedData) {
+                    return controller.buildListViewItemWidget(
+                        index, controller.tasks.where((element) => element.priority == index + 1).toList());
+                  });
+                }, childCount: 4),
+              );
+            },
           )));
         });
   }
